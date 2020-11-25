7fdc3353d6e3edcaddc49ffb90ea5849ef57bf90
roody95 <agbassahr@gmail.com>
2020-11-25 07:14:30 +0000

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { WorksComponent } from './works/works.component';
 
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch:'full' },
     { path: 'home', component: HomeComponent, data: {animation: 'home'}},
-    { path: 'services', component: HomeComponent, data: {animation: 'services'}},
-    { path: 'testimony', component: HomeComponent, data: {animation: 'testimony'}},
-    { path: 'works', component: HomeComponent, data: {animation: 'works'}},
+    { path: 'services', component: PricingComponent, data: {animation: 'services'}},
+    { path: 'testimony', component: TestimonyComponent, data: {animation: 'testimony'}},
+    { path: 'works', component: WorksComponent, data: {animation: 'works'}},
 ];
 
 @NgModule({
