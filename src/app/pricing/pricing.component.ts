bfdc2ce6ba91f11149d7d0f765ff4c440345e019
roody95 <agbassahr@gmail.com>
2020-12-04 17:55:33 +0000

    commit

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -9,14 +9,34 @@ export class PricingComponent implements OnInit {
 
   _v = false;
 
-  constructor() { }
+  currentItem;
+
+  prices = [
+    { name: 'service_1', option: [ { name:'option_A', state: false},{ name:'option_B', state: true},{ name:'option_C', state: false} ]},
+    { name: 'service_2', option: [ { name:'option_A', state: true},{ name:'option_B', state: true},{ name:'option_C', state: false}, { name:'option_D', state: true}  ]},
+    { name: 'service_3', option: [ { name:'option_A', state: false},{ name:'option_B', state: true},{ name:'option_C', state: true} ]},
+    { name: 'service_4', option: [ { name:'option_A', state: true},{ name:'option_B', state: true},{ name:'option_C', state: true},{ name:'option_D', state: false}  ]},
+    { name: 'service_5', option: [ { name:'option_A', state: true},{ name:'option_B', state: false},{ name:'option_C', state: false} ]},
+    { name: 'service_6', option: [ { name:'option_A', state: true},{ name:'option_B', state: true},{ name:'option_C', state: true},{ name:'option_D', state: true}  ]},
+    { name: 'service_7', option: [ { name:'option_A', state: false},{ name:'option_B', state: false},{ name:'option_C', state: true},{ name:'option_D', state: true}  ]},
+    { name: 'service_8', option: [ { name:'option_A', state: true},{ name:'option_B', state: true}]},
+    { name: 'service_9', option: [ { name:'option_A', state: false},{ name:'option_B', state: true}]},
+  ];
+
+ constructor() { }
 
   ngOnInit(): void {
   }
 
 
-  toogleV() {
-    this._v = !this._v;
+  toogleV(item:any) {
+    this._v = true;
+    this.currentItem = item;
+    console.log(item);
+  }
+
+  closeV() {
+    this._v = false;
   }
 
 }
