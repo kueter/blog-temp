31edd5a057d5573e61db70989202977325d86819
roody95 <agbassahr@gmail.com>
2020-12-05 19:20:12 +0000

    commit

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -12,13 +12,13 @@ export class WorksComponent implements OnInit {
   view = false;
 
   angularProjects = [
-    { name: 'Dashboard Template UI angular 9', preload: '', views: 210 , claps: 20},
-    { name: 'Blog Template UI angular 9', preload: '', views: 110 , claps: 10},
-    { name: 'E-shop Fresh Template UI angular 9 ', preload: '', views: 200 , claps: 170},
-    { name: 'E-shoes Template UI angular 9', preload: '', views: 210 , claps: 20},
-    { name: 'E-Dashboard UI dev components angular 9', preload: '', views: 100 , claps: 19},
-    { name: 'Chat Box Bot UI angular 9', preload: '', views: 1000 , claps: 409},
-    { name: 'Photo Gallery UI angular 9', preload: '', views: 780 , claps: 309}
+    { name: 'Dashboard Template UI angular 9', preload: '../../assets/img/dash1.png', views: 210 , claps: 20},
+    { name: 'Blog Template UI angular 9', preload: '../../assets/img/bl.png', views: 110 , claps: 10},
+    { name: 'E-shop Fresh Template UI angular 9 ', preload: '../../assets/img/ecom1.png', views: 200 , claps: 170},
+    { name: 'E-shoes Template UI angular 9', preload: '../../assets/img/ecos.png', views: 210 , claps: 20},
+    { name: 'Dashboard UI dev components angular 9', preload: '../../assets/img/dash2.png', views: 100 , claps: 19},
+    { name: 'Chat Box Bot UI angular 9', preload: '../../assets/img/ch.png', views: 1000 , claps: 409},
+    { name: 'Photo Gallery UI angular 9', preload: '../../assets/img/gall.png', views: 780 , claps: 309}
   ];
 
   constructor() { }
